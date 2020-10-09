06e5f85affc8155bf3cacd297a9bb45bbc9c2ad5
Cem Aydin <cem.aydin@gmx.ch>
2020-10-09 22:04:18 +0200

    collect server status and measured request time informations

--- server/dbstats.js
+++ server/dbstats.js
@@ -1,7 +1,25 @@
 import express from 'express';
+import { performance } from 'perf_hooks';
+
+async function measureRequestTime(db) {
+  const t0 = performance.now();
+  const c = await db.collection('logdata');
+  const r = await c.find({}).sort({ agent_timestamp: 1 })
+    .limit(50).toArray();
+  const t1 = performance.now();
+  return t1-t0;
+}
 
 export async function collectStats(db) {
+  // database stats
   const r = await db.stats({ scale: 1024 * 1024 });
+
+  // server status
+  const a = await db.admin();
+  const s = await a.serverStatus();
+
+  // measure request time
+  const rtime = await measureRequestTime(db);
 
   const c = db.collection('dbstats');
   const res = await c.insertOne({
@@ -9,6 +27,9 @@ export async function collectStats(db) {
     objects: r.objects,
     dataSize: r.dataSize,
     storageSize: r.storageSize,
+    perf: s.wiredTiger.perf,
+    tcmalloc: s.tcmalloc,
+    requestTime: rtime
   });
 }
 
